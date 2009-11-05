d713997e6023ee89b2380cb684e4f40fd2d3da66
christopher.ariza <christopher.ariza@13181586-94c9-11de-97c9-d5744c8abab9>
2009-11-05 04:33:47 +0000

    added tests

---
+++ athenaCL/test/baseTest.py
@@ -0,0 +1,26 @@
+import unittest, doctest
+
+
+
+def main(*testClasses):
+    '''
+    '''
+    
+    if ('noDocTest' in testClasses):
+        s1 = unittest.TestSuite()
+    else: 
+        s1 = doctest.DocTestSuite('__main__', optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+
+    for thisClass in testClasses:
+        if isinstance(thisClass, str) and thisClass == 'noDocTest':
+            pass
+        else:
+            s2 = unittest.defaultTestLoader.loadTestsFromTestCase(thisClass)
+            s1.addTests(s2) 
+    runner = unittest.TextTestRunner()
+    runner.run(s1)  
+
+if __name__ == "__main__":
+    main(Test)
+
+
---
+++ athenaCL/test/test.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+#-------------------------------------------------------------------------------
+# Name:         test.py
+# Purpose:      Controller for all tests.
+#
+# Authors:      Christopher Ariza
+#
+# License:      GPL
+#-------------------------------------------------------------------------------
+
+
+import unittest, doctest
+import os, time, imp, sys
+import traceback
+
+
+from athenaCL import athenaObj
+
+
+
+#-------------------------------------------------------------------------------
+class ModuleGather(object):
+    '''Utility class for gathering and importing all modules in the music21
+    package. 
+    '''
+    def __init__(self):
+        from music21 import base
+        self.dirParent = os.path.dirname(athenaObj.__file__)
+        self.modulePaths = []    
+        self.moduleSkip = []
+        self.pathSkip = []
+        self._walk()
+
+    def _visitFunc(self, args, dirname, names):
+        for file in names:
+            if file.endswith('py'):
+                fp = os.path.join(dirname, file)
+                if not os.path.isdir(fp):
+                    self.modulePaths.append(fp)
+
+    def _walk(self):
+        # the results of this are stored in self.curFiles, self.dirList
+        os.path.walk(self.dirParent, self._visitFunc, '')
+        self.modulePaths.sort()
+        self.modulePaths.reverse()
+
+    def _getName(self, fp):
+        '''Given full file path, find a name for the the module
+        '''
+        fn = fp.replace(self.dirParent, '') # remove parent
+        if fn.startswith(os.sep):
+            fn = fn[1:]
+        fn = fn.replace(os.sep, ':') # replace w/ dots
+        fn = fn.replace('.py', '')
+        return fn
+
+     
+    def load(self):
+#         loadPass = []
+#         loadFail = []
+#         modules = []
+#         for fp in self.modulePaths:
+#             skip = False
+#             for fnSkip in self.moduleSkip:
+#                 if fp.endswith(fnSkip):
+#                     skip = True   
+#                     break    
+#             for dirSkip in self.pathSkip:
+#                 if dirSkip in fp:
+#                     skip = True  
+#                     break
+#             if skip:
+#                 continue
+# 
+#             name = self._getName(fp)
+# 
+#             try:
+#                 mod = imp.load_source(name, fp)
+#             except Exception as excp: # this takes all exceptions!
+#                 msg = ['failed import:', fp, '\n', 
+#                     '\tEXCEPTION:', str(excp).strip()]
+#                 print(' '.join(msg))
+#                 continue
+#             modules.append(mod)
+
+        from athenaCL.libATH import unit
+        modules = []
+        modules.append(unit)
+        return modules
+
+
+
+
+
+def main(testGroup=['test']):
+    '''Run all tests. Group can be test and external
+
+    >>> print None
+    None
+    '''    
+
+    # in case there are any tests here, get a suite to load up later
+    s1 = doctest.DocTestSuite(__name__, 
+        optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+
+    modGather = ModuleGather()
+    modules = modGather.load()
+
+    print('looking for Test classes...\n')
+
+    for module in modules:
+        #print _MOD, timeStr, module
+        unitTestCases = []
+    
+        if not hasattr(module, 'Test'):
+            print('%s has no Test class' % module)
+        else:
+            if 'test' in testGroup:
+                unitTestCases.append(module.Test)
+
+        if not hasattr(module, 'TestExternal'):
+            pass
+        else:
+            if 'external' in testGroup or 'testExternal' in testGroup:
+                unitTestCases.append(module.TestExternal)
+
+        # get unittest test
+        for testCase in unitTestCases:
+            s2 = unittest.defaultTestLoader.loadTestsFromTestCase(testCase)
+            s1.addTests(s2)
+
+        try:
+            s3 = doctest.DocTestSuite(module, 
+                optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+            s1.addTests(s3)
+        except ValueError:
+            print('%s cannot load Doctests' % module)
+            continue
+    
+
+    print('running Tests...\n')
+
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(s1)  
+                 
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    # if optional command line args are given, assume they are  
+    # test group arguments
+    if len(sys.argv) >= 2:
+        main(sys.argv[1:])
+    else:
+        main()
+
+
