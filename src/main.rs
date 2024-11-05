38161343bf7f8eee412342f70b4fac0d3fe3d03b
Ales Tsurko <ales.tsurko@gmail.com>
2024-11-05 01:20:57 +0100

    Embed fira mono font

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> iced::Result {
         .settings(iced::settings::Settings {
             id: Some(app::APPLICATION_ID.to_string()),
             default_text_size: 14.into(),
-            default_font: iced::Font::MONOSPACE,
+            default_font: iced::Font::with_name("Fira Mono"),
             ..Default::default()
         })
         .window(iced::window::Settings {
@@ -18,6 +18,9 @@ fn main() -> iced::Result {
             max_size: Some((800.0, f32::MAX).into()),
             ..Default::default()
         })
+        .font(include_bytes!("../resources/fonts/Fira_Mono/FiraMono-Bold.ttf"))
+        .font(include_bytes!("../resources/fonts/Fira_Mono/FiraMono-Medium.ttf"))
+        .font(include_bytes!("../resources/fonts/Fira_Mono/FiraMono-Regular.ttf"))
         .font(iced_fonts::REQUIRED_FONT_BYTES)
         .font(iced_fonts::NERD_FONT_BYTES)
         .run()
