5d3506094380b389613629a29ee63cd657d5edf4
ales-tsurko <ales-tsurko@users.noreply.github.com>
2026-09-22 14:17:06 +0000

    deploy: 2337da8e84c7f8431d8cc197f32ecf3b0f6f9922

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fd17ca9b.js',
+                path_to_root + 'searchindex-459db0fd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
